d6d68fafee206e4392107090d2c70c057b69f764
xjx <xujinxinyw@163.com>
2020-04-09 19:48:37 +0800

    新建工程

---
+++ small_notebook_service/src/main/java/com/mex/notebook/SmallNotebookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mex.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallNotebookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallNotebookServiceApplication.class, args);
+    }
+
+}
