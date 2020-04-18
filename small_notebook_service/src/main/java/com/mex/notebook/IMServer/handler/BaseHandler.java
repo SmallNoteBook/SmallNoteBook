3209b691ea4bd090e888fef3cdda021b847aec9a
xjx <xujinxinyw@163.com>
2020-04-18 18:06:11 +0800

    更新读取方式

--- small_notebook_service/src/main/java/com/mex/notebook/IMServer/handler/BaseHandler.java
+++ small_notebook_service/src/main/java/com/mex/notebook/IMServer/handler/BaseHandler.java
@@ -2,11 +2,34 @@ package com.mex.notebook.IMServer.handler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class BaseHandler extends SimpleChannelInboundHandler<ByteBuf> {
+public class BaseHandler extends ChannelInboundHandlerAdapter {
+
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf s) throws Exception {
-        System.out.println(s);
+    public void channelRead(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
+        try {
+            ByteBuf buf = (ByteBuf)msg;
+            //创建目标大小的数组
+            byte[] barray = new byte[buf.readableBytes()];
+            //把数据从bytebuf转移到byte[]
+            buf.getBytes(0,barray);
+            //将byte[]转成字符串用于打印
+            String str=new String(barray);
+
+            if (str.length()>0)
+            {
+                System.out.println(str);
+                System.out.flush();
+            }
+            else
+            {
+                System.out.println("不能读啊");
+            }
+            buf.release();
+        }finally {
+            //buf.release();
+        }
     }
 }
