a5e2dfb430c9421fdb0a2d5f47ade9d9d997cbe0
xujinxin <xujinxinyw@163.com>
2020-05-07 18:12:29 +0800

    增加自动注入

--- small_notebook_service/src/main/java/com/mex/notebook/imessage/service/ChannelService.java
+++ small_notebook_service/src/main/java/com/mex/notebook/imessage/service/ChannelService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChannelService extends ChannelInitializer<SocketChannel> {
 
+    @Autowired
     private BaseHandlerService baseHandlerService;
 
 
